5c6ee5226e15c454538bbf0318ed36ea64f3d80a
Dylan McInnes <dylan.c.mcinnes@gmail.com>
2025-01-06 20:24:26 -0600

    Pixelate videos

--- js/pixelate.mjs
+++ js/pixelate.mjs
@@ -11,6 +11,17 @@ function setStyle(wrapped, texture, glTexture) {
 }
 
 
+async function Video_cloneTexture(wrapped, ...args) {
+  const vt = await wrapped(...args);
+  if (vt?.baseTexture?._scaleMode == undefined || !game.settings.get(MODULENAME, "avoidBlur")) {
+    return vt;
+  }
+  vt.baseTexture._scaleMode = PIXI.SCALE_MODES.NEAREST;
+  return vt;
+}
+
+
 export function register() {
   libWrapper.register("pokemon-assets", "PIXI.TextureSystem.prototype.setStyle", setStyle, "WRAPPER");
+  libWrapper.register("pokemon-assets", "game.video.cloneTexture", Video_cloneTexture, "WRAPPER");
 };
