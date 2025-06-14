53889c24dc257d7f97376885fcf7fa076d8edf92
Dylan McInnes <dylan.c.mcinnes@gmail.com>
2025-06-14 12:58:33 -0600

    Fix missing REGION_MOVEMENT_SEGMENTS

--- js/foundry/token.mjs
+++ js/foundry/token.mjs
@@ -6,6 +6,7 @@ const { Ray } = foundry.canvas.geometry;
 const { CanvasAnimation } = foundry.canvas.animation;
 const { PrimaryCanvasGroup } = foundry.canvas.groups;
 const { InvisibilityFilter } = foundry.canvas.rendering.filters;
+const { REGION_MOVEMENT_SEGMENTS } = CONST;
 
 export function NonPrivateTokenMixin(TokenClass) {
   return class NonPrivateToken extends TokenClass {
