e6a87791a7b1bd7bad71dbdf937989527410762b
Dylan McInnes <dylan.c.mcinnes@gmail.com>
2026-02-01 13:41:03 -0600

    Don't play the summon animation if the token is hidden

--- js/system-specific/dnd5e.mjs
+++ js/system-specific/dnd5e.mjs
@@ -190,6 +190,7 @@ async function ActorCry(actor) {
 async function OnCreateToken(token, options) {
   if (!game.settings.get(MODULENAME, "playSummonAnimation")) return;
   if (options.teleport || options.keepId) return; // don't play the animation if the token is teleporting
+  if (token.hidden) return; // don't play the animation if the token is hidden
   
   const actor = token.actor;
   if (!isActorPokemon(actor)) return;
