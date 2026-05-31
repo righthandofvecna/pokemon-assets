3b5e4950cb2259cdc2a858048aa9426fbf1f8e3d
Dylan McInnes <dylan.c.mcinnes@gmail.com>
2026-05-31 13:31:20 -0600

    Actually migrate signs

--- js/migrations/_index.mjs
+++ js/migrations/_index.mjs
@@ -1,5 +1,7 @@
 import { Migration_1_0_0 } from "./migration-1.0.0.mjs";
+import { Migration_1_0_2 } from "./migration-1.0.2.mjs";
 
 export const MIGRATIONS = [
   Migration_1_0_0,
+  Migration_1_0_2,
 ];
